33796a78be6f492a666405ba1b8480387b67bb30
Anshuman Verma <anshu.av97@gmail.com>
2019-06-26 15:32:03 +0530

    init :tada:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
+import { TwitterShareButton, TwitterIcon } from 'react-share'
 import './App.css';
 
 function App() {
+  const [emoji, setEmoji] = useState('👏')
+  const [text, setText] = useState('')
+
+  const formattedText = text.split(' ').join(emoji ? emoji : ' ')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Picker title='Pick your emoji…' emoji='point_up' onSelect={(e) => setEmoji(e.native)} set='twitter' />
+      <div>
+        Current Emoji - { emoji ? emoji : ''}
+      </div>
+      <input type="text" onChange={e => setText(e.target.value)} value={text}/>
+      <div>{formattedText}</div>
+      <TwitterShareButton url={true} title={formattedText}>
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
     </div>
   );
 }
