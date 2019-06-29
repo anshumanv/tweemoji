ae5f1d533eb69437cf4077a739d2658e3e9b94f7
Anshuman Verma <anshu.av97@gmail.com>
2019-06-29 19:25:17 +0530

    some style fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import 'emoji-mart/css/emoji-mart.css'
 import { Picker } from 'emoji-mart'
+import ReactSimpleTooltip from 'react-simple-tooltip'
 import { TwitterShareButton, TwitterIcon } from 'react-share'
-import './App.css';
+import './App.scss';
 
 function App() {
   const [emoji, setEmoji] = useState('👏')
@@ -11,15 +12,37 @@ function App() {
   const formattedText = text.split(' ').join(emoji ? emoji : ' ')
   return (
     <div className="App">
-      <Picker title='Pick your emoji…' emoji='point_up' onSelect={(e) => setEmoji(e.native)} set='twitter' />
-      <div>
-        Current Emoji - { emoji ? emoji : ''}
+      <div className="left">
+        <Picker title='Pick your emoji…' emoji='point_up' onSelect={(e) => setEmoji(e.native)} set='twitter' />
+        <div className="current">
+          Current Emoji: { emoji ? emoji : ''}
+        </div>
       </div>
-      <input type="text" onChange={e => setText(e.target.value)} value={text}/>
-      <div>{formattedText}</div>
-      <TwitterShareButton url={true} title={formattedText}>
-        <TwitterIcon size={32} round />
-      </TwitterShareButton>
+      <div className="right">
+        <textarea placeholder="Type your heart out" type="text" onChange={e => setText(e.target.value)} value={text}/>
+        <div className="res">{formattedText}</div>
+        <ReactSimpleTooltip
+          arrow={15}
+          background="#000"
+          border="#000"
+          color="#fff"
+          content="Tweet it!"
+          fadeDuration={0}
+          fadeEasing="linear"
+          fixed={false}
+          fontFamily="inherit"
+          fontSize="inherit"
+          offset={0}
+          padding={16}
+          placement="left"
+          radius={0}
+          zIndex={1}
+        >
+          <TwitterShareButton className="tweet" url={"https://tweemoji.netlify.com"} title={formattedText}>
+            <TwitterIcon size={64} round />
+          </TwitterShareButton>
+        </ReactSimpleTooltip>
+      </div>
     </div>
   );
 }
